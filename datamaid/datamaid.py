c4bb18c35696a32ec96bd496b451c949a3f5e956
Max Graham <maxgraham@outlook.com>
2020-06-11 21:43:05 -0300

    Added min, max, and range functions

--- datamaid/datamaid.py
+++ datamaid/datamaid.py
@@ -13,4 +13,18 @@ class TypeReport:
 
     def get_shape(self):
         return self._df.shape
-        
+    
+    def get_column_mins(self):
+        return self._df.min().rename('min')
+    
+    def get_column_maxs(self):
+        return self._df.max().rename('max')
+
+    def get_column_ranges(self):
+        result = pd.merge(
+            left=self.get_column_mins(),
+            right=self.get_column_maxs(),
+            left_index=True,
+            right_index=True
+        )
+        return result
--- tests/test_datamaid.py
+++ tests/test_datamaid.py
@@ -5,6 +5,7 @@
 
 import unittest
 import pandas as pd
+import pandas.testing as pdt
 from datamaid import TypeReport
 
 
@@ -26,3 +27,36 @@ class TestTypeReport(unittest.TestCase):
         expected = (3, 2)
         result = obj.get_shape()
         self.assertTupleEqual(result, expected)
+
+    def test_get_column_mins(self):
+        df = pd.DataFrame({
+            'col_1': [1, 2, 3],
+            'col_2': [6, 4, 5]
+        })
+        obj = TypeReport(df=df)
+        expected = pd.Series([1, 4], index=['col_1', 'col_2'], name='min')
+        result = obj.get_column_mins()
+        pdt.assert_series_equal(expected, result)
+
+    def test_get_column_maxs(self):
+        df = pd.DataFrame({
+            'col_1': [1, 2, 3],
+            'col_2': [6, 4, 5]
+        })
+        obj = TypeReport(df=df)
+        expected = pd.Series([3, 6], index=['col_1', 'col_2'], name='max')
+        result = obj.get_column_maxs()
+        pdt.assert_series_equal(expected, result)
+
+    def test_get_column_ranges(self):
+        df = pd.DataFrame({
+            'col_1': [1, 2, 3],
+            'col_2': [4, 5, 6],
+        })
+        obj = TypeReport(df=df)
+        expected = pd.DataFrame({
+            'min': [1, 4],
+            'max': [3, 6]
+        }, index = ['col_1', 'col_2'])
+        result = obj.get_column_ranges()
+        pdt.assert_frame_equal(expected, result)
